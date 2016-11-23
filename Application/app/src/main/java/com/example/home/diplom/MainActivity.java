d177f6f16287f903124ce5e434f9f9dbef9a1448
WingFlier <tiko1581@mail.ru>
2016-11-24 03:47:38 +0400

    no important changes

--- Application/app/src/main/java/com/example/home/diplom/MainActivity.java
+++ Application/app/src/main/java/com/example/home/diplom/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.home.diplom;
 
+import android.app.AlarmManager;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -14,14 +20,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,12 +40,12 @@ public class MainActivity extends AppCompatActivity {
     private Fragment fragmentNote;
     private FragmentManager manager;
     private FragmentTransaction transaction;
-    private final Calendar c = Calendar.getInstance();
+    private final Calendar calendar = Calendar.getInstance();
     private final Date d = new Date();
     private final int DIALOG_DATE = 0;
-    private int myYear = c.get(Calendar.YEAR);
-    private int myMonth = c.get(Calendar.MONTH);
-    private int myDay = c.get(Calendar.DAY_OF_MONTH);
+    private int myYear = calendar.get(Calendar.YEAR);
+    private int myMonth = calendar.get(Calendar.MONTH);
+    private int myDay = calendar.get(Calendar.DAY_OF_MONTH);
     private final int DIALOG_TIME = 1;
     private int myHour = d.getHours();
     private int myMinute = d.getMinutes();
@@ -46,10 +57,44 @@ public class MainActivity extends AppCompatActivity {
         initFab();
         initToolbar();
         initSpinner();
+        initNotification();
+
 
         manager = getSupportFragmentManager();
         fragmentRemind = new RemindFragment();
         fragmentNote = new FragmentNote();
+//        Toast.makeText(this, myHour, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+    private void initNotification() {
+
+        /***
+         * TODO code refactor especially initNotification
+         * TODO several fab-s https://www.learn2crack.com/2015/10/android-floating-action-button-animations.html
+         * TODO Navigation view burger https://youtu.be/AKSX_Ic6nLU
+         */
+
+        Context context = getApplicationContext();
+
+        Intent intent = new Intent(context, Main2Activity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        Notification.Builder builder = new Notification.Builder(context);
+
+        builder.setContentIntent(pendingIntent)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker("бла бла бла")
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setContentTitle("title of notification")
+                .setContentText("Todo thing here");
+        Notification notification = builder.build();
+
+        NotificationManager notificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
 
     }
 
@@ -75,13 +120,17 @@ public class MainActivity extends AppCompatActivity {
                     transaction.remove(fragmentNote);
                 }
                 transaction.commit();
+
             }
+
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 Toast.makeText(MainActivity.this, "Choose something", Toast.LENGTH_SHORT).show();
             }
         });
+
+
     }
 
     private void initToolbar() {
@@ -132,7 +181,9 @@ public class MainActivity extends AppCompatActivity {
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             myHour = hourOfDay;
             myMinute = minute;
-            // tvDate.setText("Time is " + myHour + " hours " + myMinute + " minutes");
+            //TextView textView = (TextView) findViewById(R.id.textView);
+            //textView.setText("Today is " + myDay + "/" + myMonth + "/" + myYear);
+
         }
     };
 
--- Application/app/src/main/java/com/example/home/diplom/RemindFragment.java
+++ Application/app/src/main/java/com/example/home/diplom/RemindFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class RemindFragment extends Fragment {
@@ -20,7 +22,12 @@ public class RemindFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_remind, container, false);
+
+
+
     }
+
 
 }
