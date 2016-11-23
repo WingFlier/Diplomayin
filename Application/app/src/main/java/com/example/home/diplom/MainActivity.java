2c003b19b24296850f9b8610d75d90101d4d3004
WingFlier <tiko1581@mail.ru>
2016-11-23 21:24:38 +0400

    App name changed. everything is ok

--- Application/app/src/main/java/com/example/home/diplom/MainActivity.java
+++ Application/app/src/main/java/com/example/home/diplom/MainActivity.java
@@ -1,52 +1,155 @@
 package com.example.home.diplom;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private Fragment fragmentRemind;
+    private Fragment fragmentNote;
+    private FragmentManager manager;
+    private FragmentTransaction transaction;
+    private final Calendar c = Calendar.getInstance();
+    private final Date d = new Date();
+    private final int DIALOG_DATE = 0;
+    private int myYear = c.get(Calendar.YEAR);
+    private int myMonth = c.get(Calendar.MONTH);
+    private int myDay = c.get(Calendar.DAY_OF_MONTH);
+    private final int DIALOG_TIME = 1;
+    private int myHour = d.getHours();
+    private int myMinute = d.getMinutes();
+
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        initFab();
+        initToolbar();
+        initSpinner();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        manager = getSupportFragmentManager();
+        fragmentRemind = new RemindFragment();
+        fragmentNote = new FragmentNote();
+
+    }
+
+    private void initSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent,
+                                       View itemSelected, int selectedItemPosition, long selectedId) {
+
+
+                if (selectedId == 0) {
+
+                    transaction = manager.beginTransaction();
+                    transaction.add(R.id.layout1, fragmentNote);
+                    transaction.remove(fragmentRemind);
+
+                } else if (selectedId == 1) {
+
+                    transaction = manager.beginTransaction();
+                    transaction.add(R.id.layout, fragmentRemind);
+                    transaction.remove(fragmentNote);
+                }
+                transaction.commit();
+            }
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(MainActivity.this, "Choose something", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+    }
+
+    private void initFab() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (fragmentRemind.isAdded()) {
+                    showDialog(DIALOG_DATE);
+
+                } else if (fragmentNote.isAdded()) {
+                    Toast.makeText(MainActivity.this, "fragment note", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_DATE) {
+            DatePickerDialog Dpd = new DatePickerDialog(this, myCallBack, myYear, myMonth, myDay);
+            return Dpd;
+        } else if (id == DIALOG_TIME) {
+            TimePickerDialog tpd = new TimePickerDialog(this, myTimeBack, myHour, myMinute, true);
+            return tpd;
+        }
+        return null;
+    }
+
+    DatePickerDialog.OnDateSetListener myCallBack = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myYear = year;
+            myMonth = monthOfYear + 1;
+            myDay = dayOfMonth;
+            //TODO: jamy u date-y Stringov stanal
+            //tvDate.setText("Today is " + myDay + "/" + myMonth + "/" + myYear);
+            showDialog(DIALOG_TIME);
+        }
+    };
+    TimePickerDialog.OnTimeSetListener myTimeBack = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myHour = hourOfDay;
+            myMinute = minute;
+            // tvDate.setText("Time is " + myHour + " hours " + myMinute + " minutes");
+        }
+    };
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+
     }
 }
