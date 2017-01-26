5f523597662690c3525dc4dd2dfda3864faf037b
Wingfly <tiko1581@mail.ru>
2017-01-26 05:12:29 -0800

    deleted first run

---
+++ Application/app/src/main/java/com/example/home/diplom/view/AboutActivity/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.example.home.diplom.view.AboutActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.home.diplom.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
